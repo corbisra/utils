66128304e306417768c6ec0f5212cd8100023faf
corbisra <jazzed_131@hotmail.com>
2026-03-08 20:35:35 +0100

    updated to latest BuildRunbook

--- src/buildrunbook.py
+++ src/buildrunbook.py
@@ -1,5 +1,6 @@
 from typing import Protocol
 import importlib
+import pandas as pd
 from baseutils.src import Processor
 global myArgs
 from baseutils.src import LoggingUtils
@@ -8,11 +9,9 @@ class BuildRunbook(Processor):
     
     def initialise(self):
        '''
-         create file and ensure path folder exists if not  create
-    
-       self.__dict__.update((key, False) for key in self.allowed_keys)
-       self.__dict__.update((key, value) for key, value in self.getkwargs() if key in self.allowed_keys)
+         create file and ensure path folder exists if not  create    
        '''
+       self.dfRunBook_ = pd.DataFrame({'Module':[], 'Class':[], 'method':[], 'comment':[]})
  
     
     def process(self):
@@ -20,7 +19,6 @@ class BuildRunbook(Processor):
          for the complete list of modules run through the aviable classes and 
          interrogate the __doc__ and __name__
        '''
-       LoggingUtils().printLog("building runbook")
        LoggingUtils().printLog("starting building runbook")
 
        for key,value in self.getkwargs().items():
@@ -31,13 +29,19 @@ class BuildRunbook(Processor):
            if isinstance(obj, type):
              print(f'{key}-isinstance')
              if isinstance(obj(), getattr(thisModule, 'Processor')):
-               methods = [ method for method in dir(obj) if '_' not in method ]
-               print(methods)
-               for m in methods:
-                 getattr(obj, m).__doc__
+               self.processClassMethods(obj, value)
         
+       print(self.dfRunBook_)
     
-    def finalise(self):
+    def finalise(self):       
        '''
          save the final file as a CSV or whatever format is required
        '''
+    
+    def processClassMethods(self, obj, packageName):
+       methods = [ method for method in dir(obj) if '_' not in method ]
+       for m in methods:
+         text=getattr(obj, m).__doc__
+         row={'Module':packageName, 'Class':obj.__name__, 'method':m, 'comment':text}
+         self.dfRunBook_ = pd.concat([self.dfRunBook_, pd.DataFrame([row])], ignore_index=True)
+         LoggingUtils().printLog(f'Class={obj.__name__}->{m}:{text}')
